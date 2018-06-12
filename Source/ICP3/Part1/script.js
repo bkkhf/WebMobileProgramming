59df216ddc94d1a299dcdb4741459839f2d93279
bkkhf <sbalachandar.k@gmail.com>
2018-06-12 16:35:12 -0500

    ICP3 first commit

---
+++ Source/ICP3/Part1/script.js
@@ -0,0 +1,54 @@
+var colors = [ "33ccff", "cc9900", "71b5c2", "cc0066", "9933ff", "e6bd01", "104393", "ca4d94"];
+function setPreviewColor(color) {
+    $('.preview').css('background-color', color);
+    $('.color-code').text($('.preview').css('background-color'));
+}
+function addBox(color) {
+    $('#colors').prepend("<div class='item' style='background-color: " + color + ";'><div>");
+}
+$(document).ready(function(){
+
+
+    $.each( colors, function( i, val ) {
+
+        addBox(val);
+    });
+
+
+    setPreviewColor(colors[Math.floor(Math.random()*colors.length)]);
+    $(document).on('keydown keyup keypress', '#color', function(){
+        color = $(this).val();
+        setPreviewColor(color);
+    })
+
+    //adding favorite color when clicks on a button #to fav-color
+    $( "#add-to-favorite" ).click(function() {
+        color = $(this).prev().val();
+		if (color != "") { 
+			if (colors.length<16) {
+				addBox(color);
+				$(this).prev().val("");
+				$(this).prev().focus();
+			}
+			else
+			{
+				$('#colors').remove("<div class='item' style='background-color: " + colors[0] + ";'><div>");
+				addBox(color);
+				$(this).prev().val("");
+				$(this).prev().focus();
+			}
+		}
+        else {
+			alert("Please enter the proper color name.");
+			document.getElementById("color").focus();
+		}
+    });
+
+   //on mouse hover
+    $(document).on('mouseover', '.item', function(){
+        color = $(this).css("background-color");
+        setPreviewColor(color);
+    })
+
+
+});
---
+++ Source/ICP3/Part2/script.js
@@ -0,0 +1,50 @@
+function getGithubInfo(user) {
+    //1. Create an instance of XMLHttpRequest class and send a GET request using it. The function should finally return the object(it now contains the response!)
+
+    return $.ajax({type: "GET",
+        url:"https://api.github.com/users/"+user,
+        async: false
+    });
+
+
+
+
+}
+//2. set the contents of the h2 and the two div elements in the div '#profile' with the user content
+function showUser(user) {
+    $('#Userprofile h2').html(user.login);
+	$('#Userprofile .id').text(user.id);
+    $('#Userprofile .avatar').text(user.avatar_url);
+    $('#Userprofile .information').text(user.html_url);
+	
+	document.getElementById("profileImg").src = user.avatar_url;
+	document.getElementById("profileURL").href = user.html_url;
+}
+//3. No user account message display
+function noSuchUser(username) {
+    
+    $("#profile h2").html(' No User');
+
+}
+
+
+$(document).ready(function(){
+    $(document).on('keypress', '#username', function(e){
+        //check if the enter(i.e return) key is pressed
+        if (e.which == 13) {
+            //get what the user enters
+            username = $(this).val();
+            //reset the text typed in the input
+            $(this).val("");
+            //get the user's information
+            response = getGithubInfo(username);
+            //if the response is successful show the user's details
+            if (response.status == 200) {
+                showUser(JSON.parse(response.responseText));
+                //else display error message
+            } else {
+                noSuchUser(username);
+            }
+        }
+    })
+});
